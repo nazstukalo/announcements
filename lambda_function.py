2539146fa72addce782faf0167a27f80306ab8d9
naz <nazar.stukalo@gmail.com>
2022-01-11 18:36:38 +0200

    first commit

---
+++ lambda_function.py
@@ -0,0 +1,12 @@
+import json
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+announcements_table = dynamodb.Table('tabmine')
+
+def lambda_handler(event, content):
+    announcements_table.put_item(Item=event['queryStringParameters'])
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Added Entry Successfully!')
+    }
